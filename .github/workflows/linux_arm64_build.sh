8e1b6ef57b0b2b1e1692b530c969594411d76d18
Raviteja Gorijala <36429068+rtg0795@users.noreply.github.com>
2026-02-19 15:41:16 -0800

    Update linux_arm64_build.sh

--- .github/workflows/linux_arm64_build.sh
+++ .github/workflows/linux_arm64_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export CC=/usr/lib/llvm-18/bin/clang
+export CXX=/usr/lib/llvm-18/bin/clang++
+export BAZEL_CXXOPTS="-stdlib=libc++"
+
 pip install --upgrade pip setuptools
 pip install --upgrade pip twine
 pip install --upgrade pip auditwheel
