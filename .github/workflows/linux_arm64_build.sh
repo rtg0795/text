0487df6d0cbc09980e7a78b0b4ca7dee82ae00cd
Raviteja Gorijala <36429068+rtg0795@users.noreply.github.com>
2026-02-19 13:46:28 -0800

    Create linux_arm64_build.sh

---
+++ .github/workflows/linux_arm64_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install --upgrade pip setuptools
+pip install --upgrade pip twine
+pip install --upgrade pip auditwheel
+pip install --upgrade patchelf
+pip install --upgrade requests
+
+pip list
+
+source ./oss_scripts/configure.sh
+
+echo "SHARED_LIBRARY_NAME" = "$SHARED_LIBRARY_NAME"
+
+bazel build --config=release_arm64_linux oss_scripts/pip_package:build_pip_package
