d1215344cfce9bd5112fecc8afee7c59d0b9978a
Raviteja Gorijala <36429068+rtg0795@users.noreply.github.com>
2026-02-23 04:00:20 -0800

    Update linux_arm64_build.sh

--- .github/workflows/linux_arm64_build.sh
+++ .github/workflows/linux_arm64_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 pip install --upgrade pip setuptools
 pip install --upgrade pip twine
@@ -12,5 +13,15 @@ source ./oss_scripts/configure.sh
 
 echo "SHARED_LIBRARY_NAME" = "$SHARED_LIBRARY_NAME"
 
-bazel build --config=release_arm64_linux oss_scripts/pip_package:build_pip_package
+gcc --version
+which gcc
+
+cat >> .bazelrc << 'EOF'
+# Override to use GCC for ARM64
+build:release_arm64_linux --repo_env=CC=gcc
+build:release_arm64_linux --repo_env=CXX=g++
+build:release_arm64_linux --repo_env=BAZEL_COMPILER=gcc
+EOF
+
+bazel build --config=release_arm64_linux --copt=-v oss_scripts/pip_package:build_pip_package --subcommands=pretty_print
 
